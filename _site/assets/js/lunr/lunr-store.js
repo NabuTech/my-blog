bda7d3743ee210b88f576c7db3427d63b98b7f74
sj <nabutech@proton.me>
2024-01-25 09:04:49 +0100

    file structure

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "My Journey in Software Development- Novice to Graduate",
+        "excerpt":"My Journey in Software Development: Novice to Graduate Epigraph ” Home is behind, the world ahead, and there are many paths to tread through shadows to the edge of night, until the stars are all alight. Then the world behind and home ahead, we’ll wander back to home and bed....","categories": [],
+        "tags": [],
+        "url": "/2023/07/22/my-journey.html",
+        "teaser": null
+      },{
+        "title": "My Journey in Software Development- Novice to Graduate",
+        "excerpt":"My Journey in Software Development: Novice to Graduate Epigraph ” Home is behind, the world ahead, and there are many paths to tread through shadows to the edge of night, until the stars are all alight. Then the world behind and home ahead, we’ll wander back to home and bed....","categories": ["myblog"],
+        "tags": [],
+        "url": "/myblog/2023/07/22/my-journey.html",
+        "teaser": null
+      }]
